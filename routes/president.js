b91e957d6d1c29809420c223dec334a21f777b4b
StadnukNazar <nazar.i.stadnyk@ukd.edu.ua>
2026-03-23 20:19:05 +0200

    Merge president data with additional fields

--- routes/president.js
+++ routes/president.js
@@ -6,7 +6,7 @@ router.get('/', async function(req, res, next) {
   const president = await db.query('SELECT * FROM presidents');
   const rowPresident = president.rows.map(s => {
     return {
-      ...p,
+      ...s,
       created_at_time: s.created_at.toLocaleTimeString(), 
       created_at_date: s.created_at.toLocaleDateString()
     }
